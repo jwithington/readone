a206fcc528d3c0e6c5b9b722325fdff19e1c5d3c
Jim Withington <jamesrwithington@gmail.com>
2015-02-05 15:35:13 -0800

    Edit feeds files

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,29 @@
 class FeedsController < ApplicationController
 	def index
-	end
+    @feeds = current_user.feeds
+  end
+ 	
+ 	def show
+  end
+ 
+	def new
+		@feed = @feeds.new
+  end
+
+  def edit
+  end
+
+  def create
+    @tfeed = current_user.feeds.new(feed_params)
+
+    # respond_to do |format|
+    #   if @todo_list.save
+    #     format.html { redirect_to @todo_list, notice: 'Todo list was successfully created.' }
+    #     format.json { render action: 'show', status: :created, location: @todo_list }
+    #   else
+    #     format.html { render action: 'new' }
+    #     format.json { render json: @todo_list.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
 end

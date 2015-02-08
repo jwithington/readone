5f6429447b280895b22f129f7d226a4d574e6da4
Jim Withington <jamesrwithington@gmail.com>
2015-02-07 16:40:47 -0800

    Edit feeds controller and feed views

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,29 +1,35 @@
 class FeedsController < ApplicationController
 	def index
-    @feeds = current_user.feeds
+    @feeds = Feed.all
   end
  	
  	def show
   end
  
 	def new
-		@feed = @feeds.new
+		@feed = Feed.new
   end
 
   def edit
   end
 
+  def destroy
+  end
+  
+  def feed_params
+    params.require(:feed).permit(:url)
+  end
   def create
-    @tfeed = current_user.feeds.new(feed_params)
+    @feed = current_user.feeds.new(feed_params)
 
-    # respond_to do |format|
-    #   if @todo_list.save
-    #     format.html { redirect_to @todo_list, notice: 'Todo list was successfully created.' }
-    #     format.json { render action: 'show', status: :created, location: @todo_list }
-    #   else
-    #     format.html { render action: 'new' }
-    #     format.json { render json: @todo_list.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_to do |format|
+      if @feed.save
+        format.html { redirect_to @feed, notice: 'Feed was successfully added.' }
+        format.json { render action: 'show', status: :created, location: @feed }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @feed.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
